df083260cada150a6e4401c3cb91292fa7cce581
SergeyMurskiy <31772372+SergeyMurskiy@users.noreply.github.com>
2019-09-21 12:10:28 +0300

    Update SearchTokens.java

--- src/main/java/com/murskiy/controller/SearchTokens.java
+++ src/main/java/com/murskiy/controller/SearchTokens.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/tokens")
+@RequestMapping(value = "/*")
 public class SearchTokens {
 
     @Autowired
